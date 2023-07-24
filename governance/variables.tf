7a58c46289880f5413fdc571e1abbd5f7c8444cf
Ariel Ramirez <Ariel.Ramirez@microsoft.com>
2023-07-24 14:23:27 -0700

    New policy code

--- governance/variables.tf
+++ governance/variables.tf
@@ -13,11 +13,6 @@ variable "policy_list" {
   description = "The list of policies to be added to the policy set."
 }
 
-variable "policy_def_list" {
-  type        = map
-  description = "The list of policies to be added to the policy set."
-}
-
 variable "policy_type" {
   type        = string
   description = "The type of the policy deployment."
@@ -26,6 +21,11 @@ variable "policy_type" {
 variable "policy_description" {
   type        = string
   description = "The description to provide for the policy assignment."
+}
+
+variable "policy_exclude_rg_id_list" {
+  type        = set(string)
+  description = "The Resources Groups that will be excluded for policy assignment with Terraform."
 }
 
 variable "policy_set_parameters" {
@@ -43,7 +43,22 @@ variable "set_audit_or_disabled" {
   description = "Set policy effect between disabled and audit"
 }
 
-variable "pip_exclude_rg_list" {
+variable "pip_exclude_rg_id_list" {
   type =  string
   description = "Resource group names to be allowed to have Public IP address (PIP)"
+}
+
+variable "blob_exclude_rg_id_list" {
+  type =  string
+  description = "Resource group names to be allowed to have Public Storage Account"
+}
+
+variable "subnet_exclude_list" {
+    type = string
+    description = "Subnet names to be excluded from audit policy to look for subnets without route tables"
+}
+
+variable "subnet_exclude_rg_list" {
+    type = string
+    description = "Resource Groups names to be excluded from audit policy to look for subnets without route tables"
 }
